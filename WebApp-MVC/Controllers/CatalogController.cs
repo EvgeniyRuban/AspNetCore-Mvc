5366db8534d5ea74472f77991fd43cc2fbed71fd
EvgeniyRuban <eugene_1997@inbox.ru>
2022-06-11 22:18:16 +0600

    Completed lesson-1 tasks.

---
+++ WebApp-MVC/Controllers/CatalogController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp_MVC.Models;
+
+namespace WebApp_MVC.Controllers
+{
+    public class CatalogController : Controller
+    {
+        private readonly Catalog _catalog;
+
+        public CatalogController(Catalog catalog)
+        {
+            _catalog = catalog;
+        }
+
+        [HttpGet]
+        public IActionResult Products() => View(_catalog.Items);
+
+        [HttpGet]
+        public IActionResult ProductAddition() => View();
+
+        [HttpPost]
+        public IActionResult ProductAddition([FromForm]Product item)
+        {
+            _catalog.Items.Add(item);
+            return View();
+        }
+    }
+}
---
+++ WebApp-MVC/Models/Catalog/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApp_MVC.Models;
+
+public sealed class Product
+{
+    public long Id { get; set; }
+    public string Title { get; set; }
+    public string? Image { get; set; }
+}
